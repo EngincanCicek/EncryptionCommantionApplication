23c6d9afa76078c5147cdd6df16f592118524768
EngincanCicek220 <engincancicek@yeterly.com>
2023-01-09 16:57:00 +0300

    Start commit

---
+++ app/src/main/java/com/deneme/encryptioncommantionapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.deneme.encryptioncommantionapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //This app just a random password generator and my first open resource kotlin app
+    }
+}
