d6693e7bc5e5e94078927335327098fb0a0ea00f
EngincanCicek <mstrengincan22tr@gmail.com>
2023-01-14 00:17:01 +0300

    Dummy update

--- app/src/main/java/com/deneme/encryptioncommantionapplication/MainActivity.kt
+++ app/src/main/java/com/deneme/encryptioncommantionapplication/MainActivity.kt
@@ -10,6 +10,5 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val dummyClass= RandomPasswordGenerator(true,true,true,18);
         println(dummyClass.randomID());
-        // test123
     }
 }
